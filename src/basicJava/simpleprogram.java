4dbb21eb680a9edb68586b71e8774ffab7199e91
Somnath Sarkar <somsarkar9698@gmail.com>
2022-07-16 22:23:41 +0530

    add this to master

---
+++ src/basicJava/simpleprogram.java
@@ -0,0 +1,12 @@
+package basicJava;
+
+public class simpleprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.format("\nCurrent Date time: %tc%n\n", System.currentTimeMillis());
+
+	}
+
+}
